718b95124ae15541ce0d50441347605a6e2a0d09
Arthur- <artthurfeter@gmail.com>
2023-10-12 23:00:07 -0300

    Adicionando código para aula 3

---
+++ todolist/src/main/java/com/fetter/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,67 @@
+package com.fetter.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fetter.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        
+        var servletPath = request.getServletPath();
+
+        if (servletPath.equals("/tasks/")) {
+             // Auth 
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            System.out.println("Authorization");
+            System.out.println(username);
+            System.out.println(password);
+
+            // Validade User
+            var user = this.userRepository.findByUsername(username);    // Gets user data from repository based on unique name
+            if (user == null) {
+                response.sendError(401);
+            }
+            else {
+                // Verify user's password
+                var verifiedPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());    // Verifies encrypted password
+
+                if (verifiedPassword.verified) {
+                    filterChain.doFilter(request, response);
+                }
+                else {
+                    response.sendError(401);
+                }
+            }
+        } 
+        else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
--- todolist/src/main/java/com/fetter/todolist/user/UserController.java
+++ todolist/src/main/java/com/fetter/todolist/user/UserController.java
@@ -2,6 +2,9 @@ package com.fetter.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,8 +29,11 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
-        var userCreated = this.userRepository.save(userModel);
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
 
+        userModel.setPassword(hashedPassword);
+
+        var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
